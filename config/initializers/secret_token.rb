d3a408b425592f516f6b4b7dfe4e14165723e258
David Waterson <david.m.waterson@gmail.com>
2011-03-09 23:08:13 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyehrApp::Application.config.secret_token = 'd04b9da62873abe57db4c4db9929c91adbf34f9c01221ad7a907c16209abac1a38f969af272b7b787ced3e09bb34cf38d92278e8e9749cea4aa3827c9fbcef66'
